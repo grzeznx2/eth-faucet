d14560b59a106f61310b86810c8b749ef25cca8c
grzeznx2 <grzeznx2@gmail.com>
2021-11-16 15:48:28 +0100

    get access to web3 provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from 'react'
+import Web3 from 'web3'
 import './App.css'
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  })
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null
+      const { ethereum, web3 } = window
+
+      if (ethereum) {
+        provider = ethereum
+      } else if (web3) {
+        provider = web3.currentProvider
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider('http://localhost:7454')
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      })
+    }
+
+    loadProvider()
+  }, [])
+
   return (
     <div className="app-wrapper">
       <div className="faucet-wrapper">
