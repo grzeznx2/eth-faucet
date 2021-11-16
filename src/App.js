0a06781d3e20ff188fec641d9dd792d117c29109
grzeznx2 <grzeznx2@gmail.com>
2021-11-16 17:24:42 +0100

    add logic for loading contract

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import { useEffect, useState } from 'react'
 import Web3 from 'web3'
 import detectEthereumProvider from '@metamask/detect-provider'
 import './App.css'
+import { loadContract } from './utils/load-contract'
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
     web3: null,
+    contract: null,
   })
   const [account, setAccount] = useState(null)
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
+      const contract = await loadContract('Faucet')
 
       if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
           provider,
+          contract,
         })
       } else {
         console.log('Please install MetaMask')
