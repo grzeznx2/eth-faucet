7799fcc2721e4fa2c23e04e58c62c1ca2e5b878f
grzeznx2 <grzeznx2@gmail.com>
2021-11-16 15:24:30 +0100

    create basic UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import './App.css'
+
 function App() {
-  return <div className>Hello</div>
+  return (
+    <div className="app-wrapper">
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
